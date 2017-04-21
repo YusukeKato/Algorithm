2bcd100ffca381cb9e771134e410fad1bf53abcc
Yusuke Kato <tq.github02480tq@gmail.com>
2017-04-21 11:44:29 +0000

    挿入ソート

---
+++ sort/inssort.py
@@ -0,0 +1,14 @@
+# 挿入ソート
+# 実行時間 : O(n^2)
+def inssort(a):
+    for i in range(1, len(a)):
+        x = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > x:
+            a[j+1] = a[j]
+            j -= 1
+        a[j+1] = x
+
+a = [4,6,2,1,8,5]
+inssort(a)
+print(a)
