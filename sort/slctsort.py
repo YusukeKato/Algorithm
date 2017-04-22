977a95953c133d0c796f3fc6b595c6211c347068
Yusuke Kato <tq.github02480tq@gmail.com>
2017-04-22 01:43:07 +0000

    選択ソート

---
+++ sort/slctsort.py
@@ -0,0 +1,16 @@
+# 選択ソート（単純選択法）
+# 実行時間　O(n^2)
+def selectsort(a):
+    for i in range(len(a)-1):
+        min = a[i]
+        k = i
+        for j in range(i+1,len(a)):
+            if a[j] < min:
+                min = a[j]
+                k = j
+        a[k] = a[i]
+        a[i] = min
+
+a = [3,7,5,1,9,3]
+selectsort(a)
+print(a)
